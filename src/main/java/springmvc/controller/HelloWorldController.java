1b9b010469c6346eee459c06ac8c4ed724c5a06c
= <=>
2019-02-18 00:26:49 +0300

    thymeleaf hello world

--- src/main/java/springmvc/controller/HelloWorldController.java
+++ src/main/java/springmvc/controller/HelloWorldController.java
@@ -1,24 +1,17 @@
 package springmvc.controller;
 
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequestMapping("/")
-public class HelloWorldController {
+public class HelloWorldController{
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-        return "welcome";
-    }
+    @GetMapping("/")
+    public String index(Model model) {
 
-    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
+        model.addAttribute("message", "Hello Spring MVC 5!");
+        model.addAttribute("target", "well done");
+        return "index";
     }
 }
